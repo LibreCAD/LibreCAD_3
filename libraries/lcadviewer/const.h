f896a155fac9a3b574ad2a57102ed86eb4a94aeb
R. van Twisk <github@rvt.dds.nl>
2014-03-04 13:15:15 -0500

    Removed switch, for now we just depend on boost::shared_ptr

--- libraries/lcadviewer/const.h
+++ libraries/lcadviewer/const.h
@@ -3,31 +3,22 @@
 
 #include "boost/math/constants/constants.hpp"
 
+namespace lc {
+    #define LAYERMANAGERHELPER_NAME "LAYERMANAGER"
+}
+
 const double PI   =   boost::math::constants::pi<double>();
 const double PI2  = 2 * boost::math::constants::pi<double>();
 
-#define USE_BOOST_LIBS
+// #define shared_ptr boost::shared_ptr
 
-#ifdef USE_BOOST_LIBS
 #include <boost/shared_ptr.hpp>
-#include <boost/enable_shared_from_this.hpp>
 #include <boost/pointer_cast.hpp>
-#include <boost/cast.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
 using boost::shared_ptr;
+using boost::dynamic_pointer_cast;
 using boost::static_pointer_cast;
 using boost::enable_shared_from_this;
 using boost::dynamic_pointer_cast;
-#else
-#include <memory>
-
-#if __GNUC__ == 4
-#include <tr1/memory>
-using std::tr1::dynamic_pointer_cast;
-using std::tr1::static_pointer_cast;
-using std::tr1::shared_ptr;
-using std::tr1::enable_shared_from_this;
-#endif
-
-#endif
-
 #endif
