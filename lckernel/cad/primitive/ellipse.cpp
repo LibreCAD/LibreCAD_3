3d8b07c7ba900643b5922a598864005e32cb292c
Dongxu Li <dli@tamaggo.com>
2014-04-10 16:39:18 -0400

    ellipse: fixed rotate

--- lckernel/cad/primitive/ellipse.cpp
+++ lckernel/cad/primitive/ellipse.cpp
@@ -21,9 +21,9 @@ shared_ptr<const lc::CADEntity> Ellipse::copy(const geo::Coordinate& offset, con
 
 shared_ptr<const lc::CADEntity> Ellipse::rotate(const geo::Coordinate& offset, const geo::Coordinate &rotation_center, const double& rotation_angle, const bool with_same_id) const {
     Ellipse* newellipse = new lc::Ellipse((this->center().rotate(rotation_center, rotation_angle)) + offset,
-                                          (this->majorP().rotate(rotation_center, rotation_angle)),
-                                          this->minorRadius() , this->startAngle() + rotation_angle,
-                                          this->endAngle() + rotation_angle);
+                                          (this->majorP().rotate(geo::Coordinate(0., 0.), rotation_angle)),
+                                          this->minorRadius() , this->startAngle(),
+                                          this->endAngle());
     if (with_same_id == 1) {
         newellipse->setID(this->id());
     }
