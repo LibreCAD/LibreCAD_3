8df5b29c39b02edd0edd7cfded8afb679caea788
R. van Twisk <github@rvt.dds.nl>
2015-01-05 11:52:12 +0100

    Allows << for Color

--- lckernel/cad/meta/color.h
+++ lckernel/cad/meta/color.h
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <iostream>
 
 namespace lc {
 
@@ -12,8 +13,10 @@ namespace lc {
     class Color {
         public:
             Color() : _r(0.), _g(0.), _b(0.), _a(1.) {}
-            Color(int r, int g, int b, int a = 0xff) : _r(std::min(std::max(0, r), 0xff) / 255.) , _g(std::min(std::max(0, g), 0xff) / 255.), _b(std::min(std::max(0, b), 0xff) / 255.), _a(std::min(std::max(0, a), 0xff) / 255.) {}
-            Color(double r, double g, double b, double a = 1.) : _r(std::min(std::max(0., r), 1.)), _g(std::min(std::max(0., g), 1.)), _b(std::min(std::max(0., b), 1.)), _a(std::min(std::max(0., a), 1.)) {}
+            Color(int r, int g, int b, int a = 0xff) : _r(std::min(std::max(0, r), 0xff) / 255.) , _g(std::min(std::max(0, g), 0xff) / 255.), _b(std::min(std::max(0, b), 0xff) / 255.), _a(std::min(std::max(0, a), 0xff) / 255.) {
+            }
+            Color(double r, double g, double b, double a = 1.) : _r(std::min(std::max(0., r), 1.)), _g(std::min(std::max(0., g), 1.)), _b(std::min(std::max(0., b), 1.)), _a(std::min(std::max(0., a), 1.)) {
+            }
             Color(const Color& other) : _r(other._r), _g(other._g), _b(other._b), _a(other._a) {}
 
             Color& operator = (const Color& other) {
@@ -59,6 +62,11 @@ namespace lc {
             }
             inline unsigned char alphaI() const {
                 return std::round(_a * 255);
+            }
+
+            friend std::ostream& operator<<(std::ostream& os, const Color& color) {
+                os << "Color(red=" << color._r << " green=" << color._g << " blue=" <<color._b << " ahlpa=" << color._a << ")";
+                return os;
             }
 
         private:
