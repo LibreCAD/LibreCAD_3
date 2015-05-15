523d0972c6f7179b4e9ea1d38398f5c1e695fa30
Kerekes Edwin <kerekes.edwin@gmail.com>
2015-05-15 14:49:14 +0200

    Removed initalizer_list from Coordine

--- lckernel/cad/geometry/geocoordinate.cpp
+++ lckernel/cad/geometry/geocoordinate.cpp
@@ -9,24 +9,3 @@ double Coordinate::angleBetween(const Coordinate& v1, const Coordinate& v2) cons
     Coordinate vEnd(v2 - (*this));
     return atan2(vStart.x() * vEnd.y() - vStart.y() * vEnd.x(), vStart.x() * vEnd.x() + vStart.y() * vEnd.y());
 }
-
-Coordinate::Coordinate(const std::initializer_list<double> &p)  {
-
-
-    auto i = p.begin();
-
-    // FIXME add debug log if the size is < 2
-    if (p.size() >= 2) {
-        _x = *i;
-        _y = *++i;
-
-        if (p.size() >= 3) {
-            _z = *++i;
-        } else {
-            _z=0.;
-        }
-    } else {
-        _x = _y = _z = 0.;
-    }
-
-}
