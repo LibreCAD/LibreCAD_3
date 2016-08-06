a99ff21e0417c1bd43be5de84ca76b25b206e7fd
gaganjyot <thegaganx@gmail.com>
2016-08-06 15:42:22 +0530

    Cleanup for Bezier/Circle/Arc

--- lckernel/cad/math/intersectionhandler.cpp
+++ lckernel/cad/math/intersectionhandler.cpp
@@ -132,11 +132,12 @@ std::vector<geo::Coordinate> Intersection::BezierCircle(
 
     } else {
 
-//        std::cout << "bezier cubic becier cubiz";
+        // get outer Bounding box
 
         geo::Area Ar = geo::Area(geo::Coordinate(C.center().x() - C.radius(), C.center().y() - C.radius()),
                          geo::Coordinate(C.center().x() + C.radius(), C.center().y() + C.radius()));
 
+        // get inner Bounding box
 
         auto newRad = C.radius() * 0.7071;
 
@@ -145,8 +146,6 @@ std::vector<geo::Coordinate> Intersection::BezierCircle(
 
         BezCir(C, Ar, minAr, B, ret);
     }
-
-    std::cout << roots.size() << std::endl;
     return ret;
 }
 
@@ -257,9 +256,8 @@ void Intersection::BezBez(const geo::BB_CSPtr B1,const geo::BB_CSPtr B2, std::ve
 }
 
 void Intersection::BezCir(const geo::Circle C, const geo::Area c_area, const geo::Area m_area, const geo::BB_CSPtr B2, std::vector<geo::Coordinate>&ret) {
-    //    std::cout << "Called";
-    auto bb2 = B2->boundingBox();
 
+    auto bb2 = B2->boundingBox();
     auto cmin = c_area.minP();
     auto cmax = c_area.maxP();
     auto bmin = bb2.minP();
@@ -267,40 +265,25 @@ void Intersection::BezCir(const geo::Circle C, const geo::Area c_area, const geo
     auto mmax = m_area.maxP();
     auto mmin = m_area.minP();
 
+    // BOUNDING BOX CHECKS. I WOULD BE HAPPY IF SOMEHOW IN FUTURE WE REPLACE THIS METHOD WITH A MATHEMATICAL METHOD.
 
     if((bmin.x() < cmin.x() && bmax.x() < cmin.x()) || (bmin.y() < cmin.y() && bmax.y() < cmin.y())
             || (bmin.x() > cmax.x() && bmax.x() > cmax.x()) || (bmin.y() > cmax.y() && bmax.y() > cmax.y())) {
         return;
     }
 
-//    if((bmin.x() < cmin.x() && bmax.x() < cmin.x()) || (bmin.y() < cmin.y() && bmax.y() < cmin.y())
-//            || (bmin.x() > cmax.x() && bmax.x() > cmax.x()) || (bmin.y() > cmax.y() && bmax.y() > cmax.y())) {
-//        return;
-//    }
-
     if(bmin.x() > mmin.x() && bmax.x() < mmax.x() && bmin.y() > mmin.y() && bmax.y() < mmax.y()) {
         return;
     }
 
-    if(bb2.height() <= BBHEURISTIC2 || bb2.width() <= BBHEURISTIC2) {
+    if(bb2.height() <= 1 || bb2.width() <= 1) {
         return;
     }
 
-
-/*    if (!((cmax.x() < bmin.x()) || (cmin.x() > bmax.x()) || (cmax.y() < bmin.y()) || (cmin.y() > bmax.y()))) {
-        auto b2split = B2->splitHalf();
-        BezCir(C, c_area, b2split[0], ret);
-        BezCir(C, c_area, b2split[1], ret);
-    } else */
-
-//    if(cmin.x() < bmin.x() && cmin.y() < bmin.y() && cmax.x() > bmax.x() && cmax.y() > bmax.y()) {
-
     if(std::fabs(C.center().distanceTo(bb2.minP()) - C.radius()) <= 10 || std::fabs(C.center().distanceTo(bb2.maxP()) - C.radius()) <= 10 ) {
-        std::cout << "hi";
         ret.push_back(B2->getCP().at(1));
         return;
     }
-//    }
     auto b2split = B2->splitHalf();
     BezCir(C, c_area, m_area, b2split[0], ret);
     BezCir(C, c_area, m_area, b2split[1], ret);
--- lcviewernoqt/drawitems/lcvline.cpp
+++ lcviewernoqt/drawitems/lcvline.cpp
@@ -1,60 +1,15 @@
 #include "lcvline.h"
 #include "../lcpainter.h"
 #include "../lcdrawoptions.h"
-#include <iostream>
 #include "cad/geometry/geovector.h"
-#include "cad/geometry/geobezier.h"
-#include "cad/geometry/geobezierbase.h"
-#include "cad/geometry/geobeziercubic.h"
-#include "cad/geometry/geocircle.h"
-#include "cad/math/intersectionhandler.h"
-#include "cad/geometry/geoarc.h"
-#include "cad/geometry/geoellipse.h"
+
 using namespace LCViewer;
 LCVLine::LCVLine(const lc::entity::Line_CSPtr line) : LCVDrawItem(true), lc::entity::Line(line, true) {
 }
 
 void LCVLine::draw(LcPainter& painter, const LcDrawOptions &options, const lc::geo::Area& rect) const {
-
     painter.move_to(start().x(), start().y());
     painter.line_to(end().x(), end().y());
-    painter.stroke();
-
-    auto c1 = lc::geo::Coordinate(0, 0);
-    auto rad = 300;
-    auto ell= lc::geo::Circle(c1, rad);
-
-    auto p1 = lc::geo::Coordinate(200,300);
-    auto p2 = lc::geo::Coordinate(0,-900);
-    auto p3 = lc::geo::Coordinate(-200,300);
-    auto p4 = lc::geo::Coordinate(-600,600);
-
-    auto bez = lc::geo::CubicBezier(p1,p2,p3, p4);
-    lc::geo::BB_CSPtr B = std::make_shared<const lc::geo::CubicBezier>(bez);
-
-    auto bb2 = B->boundingBox();
-    lc::geo::Area cc2 = lc::geo::Area(lc::geo::Coordinate(ell.center().x() - ell.radius(), ell.center().y() - ell.radius()),
-                     lc::geo::Coordinate(ell.center().x() + ell.radius(), ell.center().y() + ell.radius()));
-
-//    for(auto p : B->getCP()) {
-//        painter.point(p.x(), p.y(), 5, 1);
-//    }
-
-    painter.rectangle(bb2.minP().x(), bb2.minP().y(), bb2.maxP().x()-bb2.minP().x(), bb2.maxP().y()-bb2.minP().y());
-    painter.rectangle(cc2.minP().x(), cc2.minP().y(), cc2.maxP().x()-cc2.minP().x(), cc2.maxP().y()-cc2.minP().y());
-//    painter.stroke();
-
-    painter.move_to(p1.x(), p1.y());
-    painter.curve_to(p2.x(), p2.y(), p3.x(), p3.y(), p4.x(), p4.y());
-    painter.stroke();
-
-    auto ret = lc::maths::Intersection::BezierCircle(B, ell);
-
-    for(auto r : ret) {
-        painter.point(r.x(), r.y(), 5, 1);
-    }
-    painter.stroke();
-    painter.circle(ell.center().x(), ell.center().y(), ell.radius());
     painter.stroke();
 }
 
