8046a300f03a0dd1c4f48418a1609ec4478bdaf4
Florian ROMEO <florian.romeo@feragon.net>
2016-08-12 15:07:19 +0200

    Color select test

---
+++ unittest/ui/testcolorselect.cpp
@@ -0,0 +1,53 @@
+#include <gtest/gtest.h>
+#include "uitests.h"
+
+#include <widgets/colorselect.h>
+
+TEST(ColorSelectTest, Selection) {
+    QApplication app(argc, argv);
+    ColorSelect colorSelect(0, false, false);
+    ColorSelect colorSelect_ByBlock_ByLayer(0, true, true);
+    auto nbColors = QColor::colorNames().size();
+
+    /*
+     * Check for ByBlock and ByLayer (+ 2 for separator and custom color)
+     */
+    EXPECT_EQ(nbColors + 4, colorSelect_ByBlock_ByLayer.count());;
+    EXPECT_EQ(nbColors + 2, colorSelect.count());
+
+    /*
+     * Check color selection
+     */
+    auto lcColor = lc::Color(255, 0, 0, 255);
+    auto lcMetaColor = lc::MetaColor(lcColor);
+    colorSelect.setCurrentText("red");
+    EXPECT_TRUE(
+            lcColor.red() == colorSelect.color().red() &&
+            lcColor.green() == colorSelect.color().green() &&
+            lcColor.blue() == colorSelect.color().blue()
+    );
+    EXPECT_TRUE(
+            lcMetaColor.red() == colorSelect.metaColor()->red() &&
+            lcMetaColor.green() == colorSelect.metaColor()->green() &&
+            lcMetaColor.blue() == colorSelect.metaColor()->blue()
+    );
+}
+
+TEST(ColorSelectTest, CustomColor) {
+    QApplication app(argc, argv);
+    ColorSelect colorSelect(0, false, false);
+    lc::Color lcColor(1, 2, 3, 4);
+    lc::MetaColor lcMetaColor(lcColor);
+
+    colorSelect.setColor(lcColor);
+    EXPECT_TRUE(
+            lcColor.red() == colorSelect.color().red() &&
+            lcColor.green() == colorSelect.color().green() &&
+            lcColor.blue() == colorSelect.color().blue()
+    );
+    EXPECT_TRUE(
+            lcMetaColor.red() == colorSelect.metaColor()->red() &&
+            lcMetaColor.green() == colorSelect.metaColor()->green() &&
+            lcMetaColor.blue() == colorSelect.metaColor()->blue()
+    );
+}
