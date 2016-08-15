55fbf24cefa2824b561272619a374b8aff78bd73
Florian ROMEO <florian.romeo@feragon.net>
2016-08-15 13:24:36 +0200

    Doxygen generation fix

--- doxygen.sh
+++ doxygen.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-( cat kerneldev.dox ; echo "INPUT=lckernel" | echo "OUTPUT_DIRECTORY=doc/lckernel" ) | doxygen -
-( cat kerneldev.dox ; echo "INPUT=lcadviewer" | echo "OUTPUT_DIRECTORY=doc/lcadviewer" ) | doxygen -
-( cat kerneldev.dox ; echo "INPUT=lcadluascript" | echo "OUTPUT_DIRECTORY=doc/lcadluascript" ) | doxygen -
-( cat kerneldev.dox ; echo "INPUT=demo" | echo "OUTPUT_DIRECTORY=doc/demo" ) | doxygen -
+#This file generate Doxygen documentation
+#Maybe we should execute the following commands with CMake.
+
+(cat librecad.dox; echo "INPUT=lcadluascript"; echo "OUTPUT_DIRECTORY=doc/lcadluascript") | doxygen -
+(cat librecad.dox; echo "INPUT=lcDXFDWG"; echo "OUTPUT_DIRECTORY=doc/lcDXFDWG") | doxygen -
+(cat librecad.dox; echo "INPUT=lckernel"; echo "OUTPUT_DIRECTORY=doc/lckernel") | doxygen -
+(cat librecad.dox; echo "INPUT=lcUI"; echo "OUTPUT_DIRECTORY=doc/lcUI") | doxygen -
+(cat librecad.dox; echo "INPUT=lcviewernoqt"; echo "OUTPUT_DIRECTORY=doc/lcviewernoqt") | doxygen -
+(cat librecad.dox; echo "INPUT=lcviewerqt"; echo "OUTPUT_DIRECTORY=doc/lcviewerqt") | doxygen -
+(cat librecad.dox; echo "INPUT=luacmdinterface"; echo "OUTPUT_DIRECTORY=doc/luacmdinterface") | doxygen -
+(cat librecad.dox; echo "INPUT=unittest"; echo "OUTPUT_DIRECTORY=doc/unittest") | doxygen -
