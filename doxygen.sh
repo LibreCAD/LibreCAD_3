c190aa3c52488f0f171f804ca6c3eaaa33704af7
Florian ROMEO <florian.romeo@feragon.net>
2016-08-17 13:26:03 +0200

    Remove external libs from Doxygen

--- doxygen.sh
+++ doxygen.sh
@@ -3,9 +3,9 @@
 #This file generate Doxygen documentation
 #Maybe we should execute the following commands with CMake.
 
-(cat librecad.dox; echo "INPUT=lcadluascript"; echo "OUTPUT_DIRECTORY=doc/lcadluascript") | doxygen -
+(cat librecad.dox; echo "INPUT=lcadluascript"; echo "OUTPUT_DIRECTORY=doc/lcadluascript"; echo "EXCLUDE_PATTERNS = */lua-intf/*") | doxygen -
 (cat librecad.dox; echo "INPUT=lcDXFDWG"; echo "OUTPUT_DIRECTORY=doc/lcDXFDWG") | doxygen -
-(cat librecad.dox; echo "INPUT=lckernel"; echo "OUTPUT_DIRECTORY=doc/lckernel") | doxygen -
+(cat librecad.dox; echo "INPUT=lckernel"; echo "OUTPUT_DIRECTORY=doc/lckernel"; echo "EXCLUDE_PATTERNS = */nano-signal-slot/*") | doxygen -
 (cat librecad.dox; echo "INPUT=lcUI"; echo "OUTPUT_DIRECTORY=doc/lcUI") | doxygen -
 (cat librecad.dox; echo "INPUT=lcviewernoqt"; echo "OUTPUT_DIRECTORY=doc/lcviewernoqt") | doxygen -
 (cat librecad.dox; echo "INPUT=lcviewerqt"; echo "OUTPUT_DIRECTORY=doc/lcviewerqt") | doxygen -
