ea945638c77475c5b6bf81ee326ab2c8e746e40d
gaganjyot <thegaganx@gmail.com>
2016-08-20 16:14:18 +0530

    unit tests, docs and some future bezier utility

--- lckernel/cad/math/equation.h
+++ lckernel/cad/math/equation.h
@@ -19,20 +19,56 @@ namespace lc {
 
             Equation(Eigen::Matrix3d &mat);
 
+            /**
+             * @brief creates a new quadratic Equation
+             * @param a X^2 double value
+             * @param b Y^2 double value
+             * @param c XY double value
+             * @param d X double value
+             * @param e Y double value
+             * @param f constant double
+             */
             Equation(double a, double b, double c, double d,
                            double e, double f);
 
+            /**
+             * @brief creates a new equation with values in vector
+             * @param vec vector of quadratic equation
+             */
             Equation(const std::vector<double> &vec);
 
+            /**
+             * @brief Coefficients of the equation
+             * @return vector of coefficients.
+             */
             const std::vector<double> Coefficients() const;
 
+            /**
+             * @brief move the quadratic equation by value V
+             * @param v the coordinate value to move equation by.
+             * @return moved equation.
+             */
             const Equation move(const geo::Coordinate &v) const ;
 
+            /**
+             * @brief rotate the quadratic equation by value V
+             * @param v the angle value to rotate equation by.
+             * @return rotated equation.
+             */
             const Equation rotate(double angle) const;
 
+            /**
+             * @brief rotate the quadratic equation by value V at specific center.
+             * @param v the angle value to rotate equation by.
+             * @return rotated equation.
+             */
             const Equation rotate(const geo::Coordinate &center,
                                         double angle) const;
 
+            /**
+             * @brief Matrix
+             * @return
+             */
             const Eigen::Matrix3d Matrix() const;
 
             const Equation flipXY() const;
--- lcviewernoqt/drawitems/lcvline.cpp
+++ lcviewernoqt/drawitems/lcvline.cpp
@@ -8,8 +8,24 @@ LCVLine::LCVLine(const lc::entity::Line_CSPtr line) : LCVDrawItem(true), lc::ent
 }
 
 void LCVLine::draw(LcPainter& painter, const LcDrawOptions &options, const lc::geo::Area& rect) const {
-    painter.move_to(start().x(), start().y());
-    painter.line_to(end().x(), end().y());
+//    painter.move_to(start().x(), start().y());
+//    painter.line_to(end().x(), end().y());
+//    painter.stroke();
+
+//    painter.move_to(-1000,0);
+//    painter.line_to(1000,0);
+    painter.circle(200,0, 150);
     painter.stroke();
+
+    painter.move_to(100,-500);
+    painter.curve_to(300,1500, 500, -1500, 1000, 500);
+    painter.stroke();
+
+
+//    auto p1 = lc::geo::Coordinate(100,-500);
+//    auto p2 = lc::geo::Coordinate(300,500);
+//    auto p3 = lc::geo::Coordinate(600,-500);
+//    auto p4 = lc::geo::Coordinate(1000,500);
+
 }
 
