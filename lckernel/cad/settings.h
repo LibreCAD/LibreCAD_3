f6375f245fba6b09b5e197b5be770d44d7d70c8f
Florian Roméo <florian.romeo@feragon.net>
2018-05-01 12:45:48 +0200

    Remove Settings instance

--- lckernel/cad/settings.h
+++ lckernel/cad/settings.h
@@ -8,20 +8,12 @@ class Settings;
 class Settings
 {
 	public:
-		Settings() {
-			variable_map["LCTOLERANCE"] = 1.0e-10;
-		};
-
-		static Settings* inst;
-
 		static double val(const std::string& variable);
 
 		static void setVal(const std::string& variable,double val);
 
 		static bool exists(const std::string& variable);
 
-		static Settings* instance();
-
 	private:
-		static std::unordered_map<std::string, double> variable_map;
+		static std::unordered_map<std::string, double> _variable_map;
 };
