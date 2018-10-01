df3ffa2720d450c077235b2bc104609b80d1849a
Yori5070 <yori5070@yahoo.co.jp>
2018-10-01 11:44:02 +0900

    Add file describing style key
    In order to start normally

--- lcUI/main.cpp
+++ lcUI/main.cpp
@@ -1,19 +1,28 @@
 #include "luainterface.h"
+#include "setupCAD/itemStyle.h"
+#include "setupCAD/setupini.h"
 #include <QApplication>
-
+#include <QtCore/QDebug>
+#include <QtWidgets/QStyleFactory>
 #include <log4cxx/xml/domconfigurator.h>
+#include <QSettings>
 
 int main(int argc, char* argv[]) {
-    #if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
-        QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-    #endif
-    
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
+    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+#endif
+    // check Setup.ini file
+    SetupIni setupFile = SetupIni("/.librecad_3/setup.ini");
+    // set global style
+    ItemStyle style = ItemStyle(setupFile.getFilePath(), "Global");
+    QApplication::setStyle(QStyleFactory::create(style.getStyle()));
+
     QApplication a(argc, argv);
 
     //log4cxx::xml::DOMConfigurator::configure("Log4cxxConfig.xml");
 
     LuaInterface li;
     li.initLua();
-    
+
     return a.exec();
 }
