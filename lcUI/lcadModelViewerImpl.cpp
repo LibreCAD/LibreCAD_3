0adafc610d8bcb022abf5c17f3ccf1ccb69aa703
Rupak Bajgain <nnew234567@gmail.com>
2018-11-30 22:16:58 +0545

    Generalizing

--- lcUI/lcadModelViewerImpl.cpp
+++ lcUI/lcadModelViewerImpl.cpp
@@ -1,21 +1,23 @@
 #include "lcadModelViewerImpl.h"
 
 using namespace lc::ui;
-using namespace lc::viewer;
 
-// @TODO: result according to active layer
-LCADModelViewerImpl::LCADModelViewerImpl(QWidget* parent):_parent(parent){
+LCADModelViewerImpl::LCADModelViewerImpl(QWidget* parent):_parent(parent),_document(nullptr){
     _storageManager = std::make_shared<lc::storage::StorageManagerImpl>();
 };
 
 void LCADModelViewerImpl::setDocument(std::shared_ptr<lc::storage::Document> document){
     _document = document;
-    auto _viewer = _viewers[0];
-    _viewer->setDocument(document);
+    for(auto _viewer : _viewers){
+    	_viewer->setDocument(document);
+    };
 }
 
 LCADViewer* LCADModelViewerImpl::getViewer(){
     auto viewer = new LCADModelViewer(_parent);
     _viewers.push_back(viewer);
+    if(_document){
+    	viewer->setDocument(_document);
+    }
     return viewer;
 }
--- lcUI/lcadModelViewerImpl.h
+++ lcUI/lcadModelViewerImpl.h
@@ -1,6 +1,8 @@
 #include "lcadModelViewer.h"
 
 //This class handles the model viewers, ie. encapsulates codes for easy UI
+// @TODO:
+// Now it acts as connecting layer but later it should be QFrame consisting of multiple model views seperated by splitter
 namespace lc {
     namespace ui {
     	class LCADModelViewerImpl{
@@ -10,16 +12,19 @@ namespace lc {
     		LCADViewer* getViewer();
     		std::shared_ptr<lc::storage::Document> document(){return _document;};
     		storage::StorageManager_SPtr storageManager(){return _storageManager;};
-            viewer::manager::SnapManagerImpl_SPtr snapManager(){return _viewers[0]->snapManager();};
-            std::shared_ptr<lc::viewer::drawable::Cursor> cursor(){return _viewers[0]->cursor();};
-            viewer::drawable::TempEntities_SPtr tempEntities(){return _viewers[0]->tempEntities();};
-            viewer::manager::DragManager_SPtr dragManager(){return _viewers[0]->dragManager();};
-            storage::UndoManagerImpl_SPtr undoManager(){return _viewers[0]->undoManager();};
+            viewer::manager::SnapManagerImpl_SPtr snapManager(){return _viewers[_activeView]->snapManager();};
+            std::shared_ptr<lc::viewer::drawable::Cursor> cursor(){return _viewers[_activeView]->cursor();};
+            viewer::drawable::TempEntities_SPtr tempEntities(){return _viewers[_activeView]->tempEntities();};
+            viewer::manager::DragManager_SPtr dragManager(){return _viewers[_activeView]->dragManager();};
+            storage::UndoManagerImpl_SPtr undoManager(){return _viewers[_activeView]->undoManager();};
+            void setActiveView(int x){_activeView=x;};
+            int getActiveView(){return _activeView;};
     	private:
     		QWidget* _parent;
     		std::shared_ptr<lc::storage::Document> _document;
     		std::vector<LCADModelViewer*> _viewers;//Store all viewers
             storage::StorageManager_SPtr _storageManager;
+            int _activeView = 0;
     	};
     }
 }
