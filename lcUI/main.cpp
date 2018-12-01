723d743852d9cf7c334489e6bf3c4319ea39ffd1
Florian Roméo <florian.romeo@feragon.net>
2018-12-01 21:22:05 +0100

    This reverts commit 2d2d660f2e594452a583e2c17786923f8d856248, reversing changes made to b4adf1a87d92b5c05a9f8aa812a20501869b081c.

--- lcUI/main.cpp
+++ lcUI/main.cpp
@@ -1,6 +1,4 @@
 #include "luainterface.h"
-#include "setupCAD/itemStyle.h"
-#include "setupCAD/setupini.h"
 #include <QApplication>
 #include <QtCore/QDebug>
 #include <QtWidgets/QStyleFactory>
@@ -8,16 +6,11 @@
 #include "cad/logger/logger.h"
 
 int main(int argc, char* argv[]) {
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
-    QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
-#endif
-    // check Setup.ini file
-    SetupIni setupFile = SetupIni("setup.ini");
-    // set global style
-    ItemStyle style = ItemStyle(setupFile.getFilePath(), "Global");
-
+    #if (QT_VERSION >= QT_VERSION_CHECK(5, 4, 0))
+        QApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    #endif
+    
     QApplication a(argc, argv);
-    QApplication::setStyle(QStyleFactory::create(style.getStyle()));
 
     LOGGER;//Init logger
 
