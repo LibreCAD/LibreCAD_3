f82a14c89e074f401b6dff7947a2f888235e9398
Florian Roméo <florian.romeo@feragon.net>
2019-05-19 15:00:50 +0200

    Run clang tidy after unit tests

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -13,13 +13,24 @@ make -j2
 cd ../..
 mkdir -p build
 cd build
-cmake ..
-make -j2
+cmake -DRUN_CLANG_TIDY_PY=/usr/bin/run-clang-tidy-6.0.py -DCHECKS_SOURCE_BRANCH=${TRAVIS_BRANCH} ..
+make -j2 lcunittest
 
 if [[ $TRAVIS_TEST_RESULT != 0 ]]; then
   echo "Compilation failed"
-  cat stylecheck.out
   exit 1;
 fi
 
 ./unittest/lcunittest
+
+make stylecheck > stylecheck.out
+
+if [[ -n $(grep "warning: " stylecheck.out) ]] || [[ -n $(grep "error: " stylecheck.out) ]]; then
+  echo "Style checks failed"
+  echo ""
+  cat stylecheck.out
+  exit 1;
+else
+  echo -e "\033[1;32m\xE2\x9C\x93 passed:\033[0m $1";
+  cat stylecheck.out
+fi
