56abf9d62dd602be8140f44126955cc8bafc2973
scKorpio <kartik.sckorpio@gmail.com>
2019-08-23 17:57:06 +0530

    Folder names changed from UPPERCASE to lowercase

--- lcviewernoqt/painters/opengl/cmake/FindOpenGLRes.cmake
+++ lcviewernoqt/painters/opengl/cmake/FindOpenGLRes.cmake
@@ -1,8 +1,8 @@
-set(SHADER_PATH "${DIR}/painters/opengl/RES/SHADERS/")
-set(FONT_PATH "${DIR}/painters/opengl/RES/FONTS/TTF/")
+set(SHADER_PATH "${DIR}/painters/opengl/resources/shaders/")
+set(FONT_PATH "${DIR}/painters/opengl/resources/fonts/ttf/")
 
-configure_file(${DIR}/painters/opengl/RES/res.cpp.in 
-               ${DIR}/painters/opengl/RES/res.cpp)
+configure_file(${DIR}/painters/opengl/resources/res.cpp.in 
+               ${DIR}/painters/opengl/resources/res.cpp)
 
 message(STATUS "Shaders: ${SHADER_PATH}")
 message(STATUS "Fonts: ${FONT_PATH}")
