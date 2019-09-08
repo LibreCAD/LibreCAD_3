169859e6b003a1529ab228896790c77f69fa5260
Florian Roméo <florian.romeo@feragon.net>
2019-09-08 16:27:51 +0200

    CMake WITH_CAIRO option for lcUI

--- lcviewernoqt/painters/createpainter.cpp
+++ lcviewernoqt/painters/createpainter.cpp
@@ -1,12 +1,18 @@
 #include "createpainter.h"
+
+#ifdef WITH_CAIRO
 #include "lccairopainter.tcc"
+#endif
+
 #include <painters/opengl/openglrenderpainter.h>
 
 namespace lc {
     namespace viewer {
+#ifdef WITH_CAIRO
         LcPainter* createCairoImagePainter(unsigned char* data, const unsigned int width, const unsigned int height) {
             return new LcCairoPainter<CairoPainter::backend::Image>(data, width, height);
         }
+#endif
 
         LcPainter* createOpenGLPainter(unsigned char* data, const unsigned int width, const unsigned int height) {
            return new OpenglRenderPainter(width,height);
