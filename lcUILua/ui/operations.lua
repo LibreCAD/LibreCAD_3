e20dcc7dbfe58794a0d8c7fa5c501797af63ef8c
Jedi18 <targetakhil@gmail.com>
2020-05-13 12:31:01 +0530

    fixed polyline bug

--- lcUILua/ui/operations.lua
+++ lcUILua/ui/operations.lua
@@ -26,12 +26,10 @@ end
 --Add a cancel button in the toolbar for the current operation
 local function create_cancel_button(id)
     if(hideUI ~= true) then
-        local quickAccessTab = get_tab(id, "Quick Access")
+        local quickAccessTab = mainWindow:getToolbar():tabByName("Quick Access")
         local operationGroup = quickAccessTab:addGroup("Current operation")
 
-        local cancelButton = create_button("", ":/icons/quit.svg", "Cancel")
-        quickAccessTab:addWidget(operationGroup, cancelButton, 0, 0, 1, 1)
-        luaInterface:luaConnect(cancelButton, "pressed()", function() finish_operation(id) end)
+        mainWindow:getToolbar():addButton("", ":/icons/quit.svg", operationGroup, 0, 0, function() finish_operation(id) end, "Cancel")
     end
 end
 
@@ -41,9 +39,8 @@ luaInterface:registerEvent('finishOperation', finish)
 --Every function corresponding to the buttons in the toolbar or commands in cli widget
 function run_basic_operation(id, operation, init_method, ...)
     mainWindow:getCliCommand():setFocus()
-    --focusClicommand(id)
     finish_operation(id)
-    --create_cancel_button(id)
+    create_cancel_button(id)
     luaInterface:setOperation(id, operation(id, ...))
 	op = luaInterface:operation(id)
     if(init_method) then
@@ -57,26 +54,22 @@ function run_basic_operation(id, operation, init_method, ...)
 end
 
 function create_lw_polyline(id)
-    --focusClicommand(id)
     mainWindow:getCliCommand():setFocus()
     finish_operation(id)
     create_cancel_button(id)
 
     if(hideUI ~= true) then
-        local quickAccessTab = get_tab(id, "Quick Access")
+        local toolbar = mainWindow:getToolbar()
+        local quickAccessTab = toolbar:tabByName("Quick Access")
         local group = quickAccessTab:groupByName("Current operation")
 
-        local lineButton = create_button("", ":/icons/linesnormal.png", "Line")
-        quickAccessTab:addWidget(group, lineButton, 0, 1, 1, 1)
-        luaInterface:luaConnect(lineButton, "pressed()", function()
+        toolbar:addButton("", ":/icons/linesnormal.png", group, 0, 1, function()
             luaInterface:operation(id):createLine()
-        end)
+        end, "Line")
 
-        local arcButton = create_button("", ":/icons/arc.svg", "Arc")
-        quickAccessTab:addWidget(group, arcButton, 1, 1, 1, 1)
-        luaInterface:luaConnect(arcButton, "pressed()", function()
+        toolbar:addButton("", ":/icons/arc.svg", group, 1, 1, function()
             luaInterface:operation(id):createArc()
-        end)
+        end, "Arc")
     end
 
     luaInterface:setOperation(id, LWPolylineOperations(id))
