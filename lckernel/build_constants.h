4765bc8fd89ad0ab3e7783e64b47f0bbdb188274
Jedi18 <targetakhil@gmail.com>
2020-07-30 22:19:57 +0530

    Added export/import api for msvc compilers

--- lckernel/build_constants.h
+++ lckernel/build_constants.h
@@ -1,7 +1,15 @@
 #define VERSION_MAJOR 3
 #define VERSION_MINOR 0
 
-extern __declspec(dllimport) const char* BUILD_DATE;
-extern __declspec(dllimport) const char* BUILD_INFO;
-extern __declspec(dllimport) const char* CMAKE_VERSION;
-extern __declspec(dllimport) const char* SETTINGS_PATH;
+#ifdef Q_CC_MSVC
+#define EXPORT_API __declspec(dllexport)
+#define IMPORT_API __declspec(dllimport)
+#else
+#define EXPORT_API
+#define IMPORT_API
+#endif
+
+extern IMPORT_API const char* BUILD_DATE;
+extern IMPORT_API const char* BUILD_INFO;
+extern IMPORT_API const char* CMAKE_VERSION;
+extern IMPORT_API const char* SETTINGS_PATH;
