213cdda535d5dc4f7790d07dbcb18b0dfd143b70
Rupak Bajgain <nnew234567@gmail.com>
2020-08-04 11:41:48 +0545

    Add dxf as binary

---
+++ lckernel/build_constants.cpp
@@ -0,0 +1,7 @@
+#include <build_constants.h>
+
+EXPORT_API const char* BUILD_DATE = "2020-08-04 11:17:25";
+EXPORT_API const char* BUILD_INFO = "branch hatch - commit ea568e7c";
+EXPORT_API const char* CMAKE_VERSION = "3.16.3";
+EXPORT_API const char* SETTINGS_PATH = "/home/rupak/LibreCAD_3/build/bin";
+EXPORT_API const char* PATTERN_RESOURCE_PATH = "/home/rupak/LibreCAD_3/res/hatch";
