638edddd30645115dcd427af242b5d5e099b5404
Rupak Bajgain <nnew234567@gmail.com>
2020-08-15 19:04:54 +0545

    Connecting modules using tag

--- doxygen.sh
+++ doxygen.sh
@@ -1,18 +1,25 @@
 #!/bin/bash
-OUTPUT_DIRECTORY=doc
-MODULES=( lcadluascript persistence lckernel lcUI lcviewernoqt luacmdinterface unittest )
-EXCLUDE_PATTERNS="*/nano-signal-slot/* */tinyspline/* */libopencad/*"
-#kaguya is outside MODULES
+OUTPUT_DIRECTORY=doxygen/doc
+#doxygen is main module
+MODULES=( lckernel persistence lcviewernoqt lcadluascript lcUI luacmdinterface unittest )
+#here order matters, if cyclic 2 run
 
 #This file generate Doxygen documentation
 #Maybe we should execute the following commands with CMake.
 
-if [ ! -r "$OUTPUT_DIRECTORY" ]; then
-    echo "Creating directory $OUTPUT_DIRECTORY"
-    mkdir "$OUTPUT_DIRECTORY"
-fi
+createDirectory(){
+    if [ ! -r "$1" ]; then
+        echo "Creating directory $1"
+        mkdir "$1"
+    fi
+}
 
+createDirectory $OUTPUT_DIRECTORY
 for module in ${MODULES[@]}
 do
-    (cat librecad.dox; echo "INPUT=$module"; echo "OUTPUT_DIRECTORY=doc/$module"; echo "EXCLUDE_PATTERNS = $EXCLUDE_PATTERNS") | doxygen -
+    createDirectory $OUTPUT_DIRECTORY/$module
+    (cat $OUTPUT_DIRECTORY/../librecad.dox; echo "INPUT=$module"; echo "OUTPUT_DIRECTORY=$OUTPUT_DIRECTORY/$module"; echo "PROJECT_BRIEF=$module" ; cat doxygen/$module.cfg) | doxygen -
 done
+
+createDirectory $OUTPUT_DIRECTORY/main
+(cat $OUTPUT_DIRECTORY/../librecad.dox; echo "INPUT=doxygen"; echo "OUTPUT_DIRECTORY=$OUTPUT_DIRECTORY/main"; cat doxygen/main.cfg) | doxygen -
