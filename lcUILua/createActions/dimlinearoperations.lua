0b251d2d4e77500833aea42c858ff0a35d4835ef
Guruprasad <raneguruprasad@gmail.com>
2021-05-12 13:48:02 +0530

    DimAligned & DimLinear updated.

--- lcUILua/createActions/dimlinearoperations.lua
+++ lcUILua/createActions/dimlinearoperations.lua
@@ -20,16 +20,14 @@ setmetatable(DimLinearOperations, {
 
 function DimLinearOperations:_init()
     CreateOperations._init(self, lc.builder.DimLinearBuilder, "enterStartPoint")
-
+    self.FirstPoint = nil
+    message("<b>Dimension Linear</b>")
     message("Click on start point")
 end
 
 function DimLinearOperations:enterStartPoint(eventName, data)
-    if(eventName == "mouseMove" or eventName == "point") then
-        self.builder:setDefinitionPoint2(data["position"])
-    end
-
     if(eventName == "point") then
+        self.FirstPoint=data["position"]
         self.step = "enterEndPoint"
 
         message("Click on end point")
@@ -37,36 +35,47 @@ function DimLinearOperations:enterStartPoint(eventName, data)
 end
 
 function DimLinearOperations:enterEndPoint(eventName, data)
-    if(eventName == "mouseMove" or eventName == "point") then
-        self.builder:setDefinitionPoint3(data["position"])
-    end
-
     if(eventName == "point") then
+        self.builder:setMiddleOfText(data["position"])
+        self.builder:setDefinitionPoint2(self.FirstPoint)
+        self.builder:setDefinitionPoint3(data["position"])
+
         self.step = "enterMiddleOfText"
 
-        message("Click on text position")
+        message("Options:<u>T</u>ext or Click on text position")
+        mainWindow:cliCommand():returnText( true)
     end
 end
 
 function DimLinearOperations:enterMiddleOfText(eventName, data)
+    if(eventName == "text") then
+        if (string.lower(data["text"]) == "t" or data["text"] == "text") then
+            self.step = "enterText"
+            message("Enter text override")
+        else
+            message("Invalid Option")
+        end
+    end
     if(eventName == "mouseMove" or eventName == "point") then
         self.builder:setDefinitionPoint(data["position"])
         self.builder:setMiddleOfText(data["position"])
     end
 
     if(eventName == "point") then
-        self.step = "enterText"
-
-        mainWindow:cliCommand():returnText( true)
-
-        message("Enter dimension text (<> for value)")
+        mainWindow:cliCommand():returnText( false)
+        self:createEntity()
     end
 end
 
 function DimLinearOperations:enterText(eventName, data)
+    if(eventName == "mouseMove" or eventName == "point") then
+        self.builder:setDefinitionPoint(data["position"])
+        self.builder:setMiddleOfText(data["position"])
+    end
     if(eventName == "text") then
         mainWindow:cliCommand():returnText( false)
         self.builder:setExplicitValue(data["text"])
-        self:createEntity()
+        self.step = "enterMiddleOfText"
+        message("Click on text position")
     end
 end
