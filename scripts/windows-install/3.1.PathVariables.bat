828e2803061387cfcb5a67f22a3ad2c1d3c3f17a
Cristik24 <c.bezerdik@gmail.com>
2022-08-16 20:08:00 +0200

    moving command in a powershell script

--- scripts/windows-install/3.1.PathVariables.bat
+++
@@ -1,3 +0,0 @@
-setx QTDIR "%cd%\Qt\5.15.2\msvc2019_64" & setx QT_QPA_PLATFORM_PLUGIN_PATH "%QTDIR%\plugins\platforms\"
-
-Powershell.exe -Command "[Environment]::SetEnvironmentVariable('Path', $env:Path + ';%cd%\Qt\5.15.2\msvc2019_64;%cd%\LibreCAD_3\out\build\x64-Debug\lib;%cd%\Qt\5.15.2\msvc2019_64\bin', 'User')"
