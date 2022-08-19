470c49a9e5bc8be2cc69e09c704bdb47d9c81d4c
Cristik24 <c.bezerdik@gmail.com>
2022-08-20 00:20:36 +0200

    cmake is broken. Kitware workaround should not be needed anymore, so I'm commenting it for 20.04

--- scripts/ubuntu-install/1.install.sh
+++ scripts/ubuntu-install/1.install.sh
@@ -1,16 +1,19 @@
-sudo apt-get install qt5-default qttools5-dev qttools5-dev-tools libqt5opengl5-dev liblua5.2-dev git g++ libcairo2-dev libpango-1.0-0 libpango1.0-dev libboost-dev libboost-log-dev libboost-program-options-dev libqt5svg5-dev libgtest-dev libeigen3-dev libcurl4-gnutls-dev libgtk-3-dev libglew-dev rapidjson-dev libbz2-dev libglfw3-dev libglm-dev cmake
+sudo apt update
+sudo apt upgrade
+sudo apt install cmake qt5-default qttools5-dev qttools5-dev-tools libqt5opengl5-dev liblua5.2-dev git g++ libcairo2-dev libpango-1.0-0 libpango1.0-dev libboost-dev libboost-log-dev libboost-program-options-dev libqt5svg5-dev libgtest-dev libeigen3-dev libcurl4-gnutls-dev libgtk-3-dev libglew-dev rapidjson-dev libbz2-dev libglfw3-dev libglm-dev
 
-sudo apt remove --purge --auto-remove cmake
-sudo apt update && \
-sudo apt install -y software-properties-common lsb-release && \
-sudo apt clean all
-wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
-sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
-sudo apt update
-sudo apt install kitware-archive-keyring
-sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
-sudo apt update
-sudo apt install cmake
+
+#sudo apt remove --purge --auto-remove cmake
+#sudo apt update && \
+#sudo apt install -y software-properties-common lsb-release && \
+#sudo apt clean all
+#wget -O - https://apt.kitware.com/keys/kitware-archive-latest.asc 2>/dev/null | gpg --dearmor - | sudo tee /etc/apt/trusted.gpg.d/kitware.gpg >/dev/null
+#sudo apt-add-repository "deb https://apt.kitware.com/ubuntu/ $(lsb_release -cs) main"
+#sudo apt update
+#sudo apt install kitware-archive-keyring
+#sudo rm /etc/apt/trusted.gpg.d/kitware.gpg
+#sudo apt update
+#sudo apt install cmake
 
 cd /usr/src/gtest
 sudo cmake CMakeLists.txt
