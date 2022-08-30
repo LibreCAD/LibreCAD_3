9b3835acc8b1eceb8186c3d39637c21d458364c5
Cristik24 <c.bezerdik@gmail.com>
2022-08-30 13:02:30 +0200

    libboost also needs to be installed in debug mode. Trying to set it from conan

--- scripts/windows-install/createConanDirAndInstallDependencies.bat
+++ scripts/windows-install/createConanDirAndInstallDependencies.bat
@@ -3,6 +3,6 @@ cd conan
 conan install ..
 if NOT %errorlevel% == "0" (
 conan profile update settings.compiler.version=16  default
-conan install ..
+conan install .. -s build_type=Debug
 )
 cd ..\..
