281fa2e6c56e671a37365ffa46a0aa9ae94db28e
Cristik24 <c.bezerdik@gmail.com>
2022-08-30 14:56:15 +0200

    some packages are missing for debug, trying to let connan build those

--- scripts/windows-install/createConanDirAndInstallDependencies.bat
+++ scripts/windows-install/createConanDirAndInstallDependencies.bat
@@ -1,8 +1,8 @@
 mkdir conan
 cd conan
-conan install .. -s build_type=Debug
+conan install .. -s build_type=Debug --build missing
 if NOT %errorlevel% == "0" (
 conan profile update settings.compiler.version=16  default
-conan install .. -s build_type=Debug
+conan install .. -s build_type=Debug --build missing
 )
 cd ..\..
