c4011f09c635d10c28d31e817ea31c68eec3fd01
Cristik24 <c.bezerdik@gmail.com>
2022-08-30 13:05:01 +0200

    Both installs must be debug

--- scripts/windows-install/createConanDirAndInstallDependencies.bat
+++ scripts/windows-install/createConanDirAndInstallDependencies.bat
@@ -1,6 +1,6 @@
 mkdir conan
 cd conan
-conan install ..
+conan install .. -s build_type=Debug
 if NOT %errorlevel% == "0" (
 conan profile update settings.compiler.version=16  default
 conan install .. -s build_type=Debug
